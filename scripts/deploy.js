7da35ac958cd2acbcfeb5395c99ccc82022c53d7
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-10-30 19:46:07 +0100

    gas price

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,8 +4,7 @@ const hre = require("hardhat");
 async function main() {
   const Factory = await ethers.getContractFactory("Factory");
   // const factory = await Factory.deploy();
-  const factory = await Factory.deploy({ gasPrice: 1000000000 }); 
-
+  const factory = await Factory.deploy({ gasPrice: 1000000000 }); // Specifing the gas price in wei
 
   console.log("factory deployed to:", factory.target);
 }
